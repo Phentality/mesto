1613da963b8d003a9337a8abf8cc026cbeb6bcc8
Phentality <Pheonix09@yandex.ru>
2022-12-24 17:40:43 +0500

    AllDone

--- scripts/script.js
+++ scripts/script.js
@@ -1,54 +1,37 @@
 let popup = document.querySelector('.popup');
 let popupClose = popup.querySelector('.popup__close');
 let popupOpen = document.querySelector('.profile__info-button');
+let formElement = document.querySelector('.popup__container');
+let nameInput = formElement.querySelector('.popup__name');
+let jobInput = formElement.querySelector('.popup__profession');
+let formSave = formElement.querySelector('.popup__save');
+let profileInfo = document.querySelector('.profile__info');
+let profileInfoName = profileInfo.querySelector('.profile__info-name');
+let profileInfoProfession = profileInfo.querySelector('.profile__info-profession');
 
 function openPopup () {
     popup.classList.add('popup_opened');
+
 }
 
-function closePopup() {
+function closePopup () {
     popup.classList.remove('popup_opened');
 }
 
 popupOpen.addEventListener('click', openPopup);
 popupClose.addEventListener('click', closePopup);
 
-let save = popup.querySelector('.popup__save');
-let nameInput = popup.querySelector('.popup__name');
-let jobInput = popup.querySelector('.popup__profession');
-
 function savePopup () {
-    nameInput.textContent()
+    profileInfoName.textContent = nameInput.value;
+    profileInfoProfession.textContent = jobInput.value;
+    closePopup();
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+formSave.addEventListener('click', savePopup);
 
 /*function handleFormSubmit (evt) {
     evt.preventDefault();
-    nameInput = nameInput.value;
-    jobInput = jobInput.value;
-    
-    // Получите значение полей jobInput и nameInput из свойства value
-
-    // Выберите элементы, куда должны быть вставлены значения полей
-
-    // Вставьте новые значения с помощью textContent
+    profileInfoName.textContent = nameInput.value;
+    profileInfoProfession.textContent = jobInput.value;
 }
-
-// Прикрепляем обработчик к форме:
-// он будет следить за событием “submit” - «отправка»
-popup.addEventListener('submit', handleFormSubmit); */
+formElement.addEventListener('submit', handleFormSubmit); */
