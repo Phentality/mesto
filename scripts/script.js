b107c8bac545932f0f5907bb9ad627c59e5831ee
Phentality <Pheonix09@yandex.ru>
2022-12-26 23:44:30 +0500

    ВтораяИтерация

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,17 @@
 let popup = document.querySelector('.popup');
 let popupClose = popup.querySelector('.popup__close');
 let popupOpen = document.querySelector('.profile__info-button');
-let formElement = document.querySelector('.popup__container');
-let nameInput = formElement.querySelector('.popup__name');
-let jobInput = formElement.querySelector('.popup__profession');
-let formSave = formElement.querySelector('.popup__save');
+formElement = document.querySelector('.popup__form');
+let nameInput = formElement.querySelector('.popup__input_type_name');
+let jobInput = formElement.querySelector('.popup__input_type_profession');
 let profileInfo = document.querySelector('.profile__info');
 let profileInfoName = profileInfo.querySelector('.profile__info-name');
 let profileInfoProfession = profileInfo.querySelector('.profile__info-profession');
 
 function openPopup () {
-    popup.classList.add('popup_opened');
+    popup.classList.add('popup_opened')
+    profileInfoName.textContent = nameInput.value;
+    profileInfoProfession.textContent = jobInput.value;
 
 }
 
@@ -27,11 +28,11 @@ function savePopup () {
     closePopup();
 }
 
-formSave.addEventListener('click', savePopup);
-
-/*function handleFormSubmit (evt) {
+function handleFormSubmit (evt) {
     evt.preventDefault();
     profileInfoName.textContent = nameInput.value;
     profileInfoProfession.textContent = jobInput.value;
+    closePopup();
 }
-formElement.addEventListener('submit', handleFormSubmit); */
+
+formElement.addEventListener('submit', handleFormSubmit);
